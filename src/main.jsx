4abe88a106f5f9f80b52ae037d8c711a1af68a04
Alan <alan_hahn1995@outlook.com>
2025-02-07 15:45:02 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />} >
+          <Route path="/" element={<Home />} />
+          <Route path="/movie:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
