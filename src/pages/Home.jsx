337e034cfe75201d004250e9761463c96f05dddb
Alan <alan_hahn1995@outlook.com>
2025-02-10 16:16:22 -0300

    fazendo conexão com a api de filmes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,24 @@
+import { useState, useEffect } from "react";
+
+const moviesUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
+    const [topMovies, setTopMovies] = useState([]);
+
+    const getTopRatedMovies = async (url) => {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setTopMovies(data.results);
+    };
+
+    useEffect(() => {
+        const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
+
+        getTopRatedMovies(topRatedUrl);
+    }, []);
+
     return (
         <div>
             <h1>Home</h1>
