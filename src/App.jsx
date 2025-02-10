e541573382608c3b71f915ae28bd412f00e17ec9
Alan <alan_hahn1995@outlook.com>
2025-02-10 15:23:49 -0300

    Criação das páginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-
-import './App.css'
+import { Link, Outlet } from 'react-router-dom';
+import './App.css';
 
 function App() {
   
 
   return (
     <div className="App">
+      <nav id="navBar">
+        <h2>
+          <Link to="/">MoviesLib</Link>
+        </h2>
+        <Link to="/movie/1">Movie</Link>
+        <Link to="/search">Search</Link>
+      </nav>
       <h1>Welcome to Movies Library</h1>
+      <Outlet /> 
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
