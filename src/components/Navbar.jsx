fe16c54374700ad251c127ba0e12ac023e7bcc6b
Alan <alan_hahn1995@outlook.com>
2025-02-12 12:53:08 -0300

    criado o estilo para navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+import './Navbar.css';
 const Navbar = () => {
     return (
-        <nav id="navBar">
+        <nav id="navbar">
         <h2>
           <Link to="/"><BiCameraMovie /> MoviesLib </Link>
         </h2>
